b0ac7062c13f3389b0c08e377bd3a63dad201bb6
CarlErnest0707 <2025-1-01107@lpu.edu.ph>
2025-09-24 18:33:19 +0800

    Update While loop count-up example 1.c

--- While loop count-up example 1.c
+++ While loop count-up example 1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
    int a = 0;
-   printf("This program prints 1 - 1"); //i inserted the number "1 - 1" instead of "1 - 10" because upon running, it actually shows "Count 1 - 10". 
+   printf("This program prints 1 - 10 \n"); //i inserted the number "1 - 1" instead of "1 - 10" because upon running, it actually shows "Count 1 - 10". 
     while (a < 11) {
         printf("%d\n", a);
         a++;

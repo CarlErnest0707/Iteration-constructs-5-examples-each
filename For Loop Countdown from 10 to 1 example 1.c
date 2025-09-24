6d0453242e8a980a178e8965ddb6b78e50cbbcd5
CarlErnest0707 <2025-1-01107@lpu.edu.ph>
2025-09-24 21:07:57 +0800

    Add files via upload

---
+++ For Loop Countdown from 10 to 1 example 1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("This Program demonstrates a countdown from 10 to 1 \n");
+    for(int b = 10; b >= 1; b--) {
+        printf("%d\n", b);
+    }
+    return 0;
+}

5454fe893c8357c26f75756b42035be4425a1363
CarlErnest0707 <2025-1-01107@lpu.edu.ph>
2025-09-24 21:44:55 +0800

    Add files via upload

---
+++ For loop Squared number example 4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    printf("This Program shows you the squared of the said number \n");
+    for(int d = 2; d <= 10; d++) {
+        printf("%d squared is %d\n", d, d*d);
+        
+    }
+    return 0;
+}

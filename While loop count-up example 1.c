c1f398ebe613fe48336bfce4110a0a54bed4f823
CarlErnest0707 <2025-1-01107@lpu.edu.ph>
2025-09-24 18:26:34 +0800

    Add files via upload

---
+++ While loop count-up example 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+   int a = 0;
+   printf("This program prints 1 - 1"); //i inserted the number "1 - 1" instead of "1 - 10" because upon running, it actually shows "Count 1 - 10". 
+    while (a < 11) {
+        printf("%d\n", a);
+        a++;
+    }
+    return 0;
+}
